b68389cf2ee0b0b97903470629e80c0096f6f5cb
Adrian Hesketh <adrianhesketh@hushmail.com>
2023-06-30 17:55:48 +0100

    feat: add xc

--- flake.nix
+++ flake.nix
@@ -1,6 +1,10 @@
 {
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.05";
+    xc = {
+      url = "github:joerdav/xc";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   # az (Azure CLI)
@@ -11,14 +15,15 @@
   # ranger (file manager)
   # vim
 
-  outputs = { self, nixpkgs }: {
+  outputs = { self, nixpkgs, xc }: {
     packages."x86_64-linux".default = let
       pkgs = nixpkgs.legacyPackages."x86_64-linux";
-    in pkgs.buildEnv {
-      name = "home-packages";
-      paths = with pkgs; [
-        cowsay
-      ];
-    };
+      in pkgs.buildEnv {
+        name = "packages";
+        paths = with pkgs; [
+          cowsay
+          xc.packages."x86_64-linux".xc
+        ];
+      };
   };
 }
